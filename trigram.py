5d9192e95bdf7fa11345451a41b78077d7b482e3
Tian Wei <ch.tian@outlook.com>
2018-06-12 05:14:40 +0000

    tri-gram language model tool.

--- trigram.py
+++ trigram.py
@@ -1,25 +1,23 @@
 #encoding=utf-8
 
-'''
-'''
-
 import math
 import jieba
 import cPickle as pickle
 
 
 class Trigram:
-    
-    self.voc_path = ''
-    self.model_path = ''
 
     def __init__(self):
+        print 'initializing tri-gram model.'
+        self.model_path = './model/trigram.pkl'
+        self.voc_path = './model/vocab.txt'
 
         # help words
         self.unk_word = '<unk>'
         self.frt_word = '<s1>'
         self.scd_word = '<s2>'
         self.lst_word = '</tail>'
+        self.appr_zero = 1.0 / 10000
 
         # load vocabulary
         voc_file = open(self.voc_path, 'r')
@@ -33,7 +31,7 @@ class Trigram:
         for w in help_words:
             self.w2id[w] = idx
             idx += 1
-        print('%d words loaded.' % len(w2id))
+        print('%d words loaded.' % len(self.w2id))
 
         # load trigram model
         model_file = open(self.model_path, 'rb')
@@ -50,10 +48,22 @@ class Trigram:
         print('discount for bi-gram,  D1: %.4f, D2: %.4f, D3: %.4f.' % (self.bgD1, self.bgD2, self.bgD3))
         print('discount for tri-gram, D1: %.4f, D2: %.4f, D3: %.4f.' % (self.tgD1, self.tgD2, self.tgD3))
 
+        print 'tri-gram model initialized.'
+
 
     def log_probability_sentence(self, sentence):
+        """
+        Compute the log-probability of the given sentence
+            based on tri-gram language model.
+        Arg:
+            sentence: The given sentence.
+        Return:
+            The log-probability of the given sentence.
+        """
         sentence = ''.join(sentence.split())
-        words = [self.frt_word, self.scd_word] + jieba.cut(sentence) + [self.lst_word]
+        words = jieba.lcut(sentence)
+        words = [w.encode('utf-8') for w in words]
+        words = [self.frt_word, self.scd_word] + words + [self.lst_word]
         word_ids = [self.word2id(w, self.w2id) for w in words]
         res = 0.0
         for i in range(len(word_ids)-2):
@@ -81,7 +91,8 @@ class Trigram:
             probability_discounted = max(xijk-delta, 0) / xij_
             lmda = delta * nij_ / xij_
         res = probability_discounted + lmda * self.probability_word_given_word(widm, widr)
-        assert not 0.0 == res
+        #assert not 0.0 == res
+        if 0.0 == res: res = self.appr_zero
         return math.log(res)
 
     def probability_word_given_word(self, widl, widr):
@@ -112,7 +123,7 @@ class Trigram:
         if wid in self.ug2n_r:
             n_j = self.ug2n_r[wid]
         res = float(n_j) / self.bg_num
-        assert not 0.0 == res
+        #assert not 0.0 == res
         return res
 
     def word2id(self, word, word_dict):
